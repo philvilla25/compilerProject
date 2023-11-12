ceba7789e6e9c142bd49cca82ab62d216b71aaa8
philvilla25 <114821553+philvilla25@users.noreply.github.com>
2023-11-11 19:42:14 -0500

    Delete Assignment_2 directory

--- Assignment_2/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Assign2Test"},{"p":"<Unnamed>","l":"Coffee"},{"p":"<Unnamed>","l":"FoodItem"},{"p":"<Unnamed>","l":"Fruit"},{"p":"<Unnamed>","l":"Inventory"},{"p":"<Unnamed>","l":"Preserve"},{"p":"<Unnamed>","l":"Vegetable"}];updateSearchResults();
